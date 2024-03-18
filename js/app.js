3a10b5beedc0dc4a3decc6ec47928520e830a1f0
valeriogonini <valeriogonini@gmail.com>
2024-03-18 16:24:42 +0100

    add milstone 1

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const Membri = [
+    {
+        name: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
+
+    },
+    {
+        name: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+
+    },
+    {
+        name: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+
+    },
+    {
+        name: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+
+    },
+    {
+        name: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+
+    },
+    {
+        name: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+
+    },
+]
+
+for (let i = 0; i < Membri.length; i++) {
+     MembriTeam = Membri[i]
+     console.log(MembriTeam);
+
+     
+}
+
